8fccb44834b934ca12fac97e115f1b1ac4bc457e
Aayush Jain <aayush.shah31@gmail.com>
2020-08-14 11:53:09 +0530

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        double a = 7.0, b = 0;
+    //        double c;
+    //        try
+    //        {
+    //            c = a / b;
+    //        }
+    //        catch(DivideByZeroException D)
+    //        { 
+    //            Console.WriteLine("Division By Zero Not Allowed : {0} " , D.Message);
+    //            Console.Read();
+    //        }       
+    //    }
+    //}
+
+    class DivByZero : Exception
+    {
+        public DivByZero()
+        {
+            Console.Write("Exception Has Occurred : Do Not Divide By Zero! ");
+        }
+    }
+
+    class Program
+    { 
+        public double DivisionOperation(double n , double d)
+        {
+            if (d == 0)
+            {
+                throw new DivByZero(); //Throw Custom/User made Exception
+            } 
+            return n / d;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            double num = 9, den = 0, quotient;
+            try
+            { 
+                quotient = obj.DivisionOperation(num, den);
+                Console.WriteLine("Quotient = {0}", quotient);
+            } 
+            catch (Exception e)
+            { 
+                Console.WriteLine(e.Message);
+                //Console.Read();
+            }
+            finally
+            {
+                Console.WriteLine("Finally This Happens!!!");
+                Console.Read();
+            }
+            Console.WriteLine("Hello!!!"); //Code After Finally Can Execute
+        }
+    }
+
+}
+
